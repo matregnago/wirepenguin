0633da3869e386868a364f81d1eaf95632e00718
Matheus Tregnago <maugustotreg@gmail.com>
2025-03-24 16:51:04 -0300

    starting transport layer packets

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use pnet::datalink;
 use pnet::datalink::Channel::Ethernet;
+use pnet::packet::arp::ArpPacket;
 use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::ipv6::Ipv6Packet;
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::udp::UdpPacket;
 use pnet::packet::Packet;
 use std::env;
 
@@ -25,15 +30,52 @@ fn main() {
         match rx.next() {
             Ok(packet) => {
                 let packet = EthernetPacket::new(packet).unwrap();
-                if packet.get_ethertype() == EtherTypes::Ipv4 {
-                    let ipv4packet = Ipv4Packet::new(packet.payload()).unwrap();
-                    println!(
-                        "IPv4 packet: source {} destination {} => {} {}",
-                        ipv4packet.get_source(),
-                        ipv4packet.get_destination(),
-                        ipv4packet.get_next_level_protocol(),
-                        ipv4packet.get_total_length(),
-                    );
+                match packet.get_ethertype() {
+                    EtherTypes::Ipv4 => {
+                        let ipv4packet = Ipv4Packet::new(packet.payload()).unwrap();
+                        match ipv4packet.get_next_level_protocol() {
+                            IpNextHeaderProtocols::Udp => {
+                                let udppacket = UdpPacket::new(packet.payload()).unwrap();
+                                println!(
+                                    "UDP Packet: {} -> {} length: {}",
+                                    udppacket.get_source(),
+                                    udppacket.get_destination(),
+                                    udppacket.get_length()
+                                )
+                            }
+                            IpNextHeaderProtocols::Tcp => {
+                                let tcppacket = TcpPacket::new(packet.payload()).unwrap();
+                                println!(
+                                    "TCP Packet: {} -> {}",
+                                    tcppacket.get_source(),
+                                    tcppacket.get_destination()
+                                )
+                            }
+                            _ => continue,
+                        }
+                    }
+                    EtherTypes::Ipv6 => {
+                        let ipv6packet = Ipv6Packet::new(packet.payload()).unwrap();
+                        println!(
+                            "IPv6 packet: source {} destination {} => {}",
+                            ipv6packet.get_source(),
+                            ipv6packet.get_destination(),
+                            ipv6packet.get_next_header()
+                        );
+                    }
+                    EtherTypes::Arp => {
+                        let arppacket = ArpPacket::new(packet.payload()).unwrap();
+                        println!("ARP packet: {:?}", arppacket);
+                    }
+                    _ => {
+                        println!(
+                            "Unknown packet: {} > {}; ethertype: {:?} length: {}",
+                            packet.get_source(),
+                            packet.get_destination(),
+                            packet.get_ethertype(),
+                            packet.packet().len()
+                        );
+                    }
                 }
             }
 
