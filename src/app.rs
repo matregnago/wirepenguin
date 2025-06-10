3e9a393cb1eb0f0c738506c82352ac7a1e9026c8
Matheus Tregnago <maugustotreg@gmail.com>
2025-06-09 23:38:19 -0300

    chore: idk wtf just happened

--- src/app.rs
+++ src/app.rs
@@ -12,13 +12,7 @@ use pnet::{
     datalink::{self, NetworkInterface},
 };
 use ratatui::{
-    layout::{Alignment, Constraint, Flex, Layout, Margin, Rect},
-    style::{Color, Modifier, Style},
-    text::{Line, Span, Text},
-    widgets::{
-        BarChart, Block, Borders, Cell, Clear, Padding, Paragraph, Row, Scrollbar,
-        ScrollbarOrientation, ScrollbarState, Table, TableState,
-    },
+    widgets::{ScrollbarState, TableState},
     DefaultTerminal, Frame,
 };
 use std::{
@@ -209,242 +203,10 @@ impl App {
             &mut self.interfaces_scroll_state,
         );
     }
-    pub fn next_row(&mut self) {
-        let i = match self.table_state.selected() {
-            Some(i) => {
-                if self.packets.is_empty() || i >= self.packets.len() - 1 {
-                    0
-                } else {
-                    i + 1
-                }
-            }
-            None => 0,
-        };
-        self.table_state.select(Some(i));
-        self.scroll_state = self.scroll_state.position(i);
-    }
 
-    pub fn previous_row(&mut self) {
-        let i = match self.table_state.selected() {
-            Some(i) => {
-                if i == 0 {
-                    if self.packets.len() > 0 {
-                        self.packets.len() - 1
-                    } else {
-                        0
-                    }
-                } else {
-                    i - 1
-                }
-            }
-            None => 0,
-        };
-        self.table_state.select(Some(i));
-        self.scroll_state = self.scroll_state.position(i);
-    }
-
-    fn generate_ref_array(&self, complete_packet: &CompletePacket) -> Option<[String; 5]> {
-        if let Some(layer2) = &complete_packet.layer_2 {
-            let (src_ip, dst_ip) = match layer2 {
-                PacketsData::ArpPacket(arp_packet) => {
-                    return Some([
-                        complete_packet.id.to_string(),
-                        "ARP".to_string(),
-                        format!(
-                            "{} ({})",
-                            arp_packet.sender_proto_addr, arp_packet.sender_hw_addr
-                        ),
-                        format!(
-                            "{} ({})",
-                            arp_packet.target_proto_addr, arp_packet.target_hw_addr
-                        ),
-                        arp_packet.length.to_string(),
-                    ]);
-                }
-                PacketsData::Ipv4Packet(ipv4) => {
-                    (ipv4.source.to_string(), ipv4.destination.to_string())
-                }
-                PacketsData::Ipv6Packet(ipv6) => {
-                    (ipv6.source.to_string(), ipv6.destination.to_string())
-                }
-                _ => ("".to_string(), "".to_string()),
-            };
-
-            if let Some(layer3) = &complete_packet.layer_3 {
-                match layer3 {
-                    PacketsData::TcpPacket(tcp) => {
-                        return Some([
-                            complete_packet.id.to_string(),
-                            "TCP".to_string(),
-                            format!("{}:{}", src_ip, tcp.source),
-                            format!("{}:{}", dst_ip, tcp.destination),
-                            tcp.length.to_string(),
-                        ]);
-                    }
-                    PacketsData::UdpPacket(udp) => {
-                        return Some([
-                            complete_packet.id.to_string(),
-                            "UDP".to_string(),
-                            format!("{}:{}", src_ip, udp.source),
-                            format!("{}:{}", dst_ip, udp.destination),
-                            udp.length.to_string(),
-                        ]);
-                    }
-                    PacketsData::IcmpPacket(icmp) => {
-                        return Some([
-                            complete_packet.id.to_string(),
-                            "ICMP".to_string(),
-                            src_ip,
-                            dst_ip,
-                            icmp.length.to_string(),
-                        ]);
-                    }
-                    PacketsData::Icmpv6Packet(icmpv6) => {
-                        return Some([
-                            complete_packet.id.to_string(),
-                            "ICMPv6".to_string(),
-                            src_ip,
-                            dst_ip,
-                            icmpv6.length.to_string(),
-                        ]);
-                    }
-                    _ => {}
-                }
-            }
-        }
-        None
-    }
-
-    fn render_table(&mut self, frame: &mut Frame, area: Rect) {
-        let header = ["ID", "Protocolo", "Origem", "Destino", "Length"]
-            .into_iter()
-            .map(Cell::from)
-            .collect::<Row>();
-
-        let rows: Vec<Row> = self
-            .packets
-            .iter()
-            .filter_map(|data| {
-                self.generate_ref_array(data).map(|item| {
-                    item.into_iter()
-                        .map(|content| Cell::from(Text::from(format!("\n{content}\n"))))
-                        .collect::<Row>()
-                        .height(2)
-                })
-            })
-            .collect();
-
-        let widths = [
-            Constraint::Length(8),
-            Constraint::Length(10),
-            Constraint::Length(25),
-            Constraint::Length(25),
-            Constraint::Length(8),
-        ];
-        let bar = " > ";
-        let table = Table::new(rows, widths)
-            .header(header)
-            .block(Block::new().borders(Borders::ALL))
-            .highlight_symbol(Text::from(vec![
-                "".into(),
-                bar.into(),
-                bar.into(),
-                "".into(),
-            ]));
-
-        frame.render_stateful_widget(table, area, &mut self.table_state.clone());
-    }
-
-    fn render_interfaces(&mut self, frame: &mut Frame, area: Rect) {
-        let table = self.make_table();
-        frame.render_widget(table, area);
-    }
-
-    fn make_table(&mut self) -> Table {
-        let header = Row::new(vec!["", "Nome", "MAC", "Ipv4", "Ipv6"])
-            .style(Style::default().fg(Color::Yellow))
-            .height(1);
-        let mut rows = Vec::new();
-        for w in &self.interfaces {
-            let mut active = String::from("");
-            if self.interface.is_some() && self.interface.clone().unwrap() == *w {
-                active = String::from("*");
-            }
-            let name = if cfg!(windows) {
-                w.description.clone()
-            } else {
-                w.name.clone()
-            };
-            let mac = w.mac.unwrap_or(MacAddr::default()).to_string();
-            let ipv4: Vec<Line> = w
-                .ips
-                .iter()
-                .filter(|f| f.is_ipv4())
-                .cloned()
-                .map(|ip| {
-                    let ip_str = ip.ip().to_string();
-                    Line::from(vec![Span::styled(
-                        format!("{ip_str:<2}"),
-                        Style::default().fg(Color::Blue),
-                    )])
-                })
-                .collect();
-            let ipv6: Vec<Span> = w
-                .ips
-                .iter()
-                .filter(|f| f.is_ipv6())
-                .cloned()
-                .map(|ip| Span::from(ip.ip().to_string()))
-                .collect();
-
-            let mut row_height = 1;
-            if ipv4.len() > 1 {
-                row_height = ipv4.clone().len() as u16;
-            }
-            rows.push(
-                Row::new(vec![
-                    Cell::from(Span::styled(
-                        format!("{active:<1}"),
-                        Style::default().fg(Color::Red),
-                    )),
-                    Cell::from(Span::styled(
-                        format!("{name:<2}"),
-                        Style::default().fg(Color::Green),
-                    )),
-                    Cell::from(mac),
-                    Cell::from(ipv4.clone()),
-                    Cell::from(vec![Line::from(ipv6)]),
-                ])
-                .height(row_height),
-            );
-        }
-
-        let table = Table::new(
-            rows,
-            [
-                Constraint::Length(1),
-                Constraint::Length(8),
-                Constraint::Length(18),
-                Constraint::Length(14),
-                Constraint::Length(25),
-            ],
-        )
-        .header(header)
-        .block(
-            Block::default()
-                .title(Line::from(vec![
-                    Span::styled("|Inter", Style::default().fg(Color::Yellow)),
-                    Span::styled("f", Style::default().fg(Color::Red)),
-                    Span::styled("aces|", Style::default().fg(Color::Yellow)),
-                ]))
-                .border_style(Style::default().fg(Color::Rgb(100, 100, 100)))
-                .title_style(Style::default().fg(Color::Yellow))
-                .title_alignment(Alignment::Right)
-                .borders(Borders::ALL)
-                .padding(Padding::new(0, 0, 1, 0)),
-        )
-        .column_spacing(1);
-        table
+    fn render_popup(&self, frame: &mut Frame) {
+        let popup_widget = PopupWidget::new(&self.selected_popup_packet);
+        popup_widget.render(frame, frame.area());
     }
 
     fn next_active_interface(&mut self) {
@@ -467,78 +229,15 @@ impl App {
             self.sniffer.start();
         }
     }
-    fn get_last_layer_protocol_name(packet: &CompletePacket) -> Option<&'static str> {
-        if let Some(layer3) = &packet.layer_3 {
-            match layer3 {
-                PacketsData::TcpPacket(_) => Some("TCP"),
-                PacketsData::UdpPacket(_) => Some("UDP"),
-                PacketsData::IcmpPacket(_) => Some("ICMP"),
-                PacketsData::Icmpv6Packet(_) => Some("ICMPv6"),
-                _ => None,
+}
+
+pub fn handle_input_events(tx: mpsc::Sender<Event>) {
+    loop {
+        match crossterm::event::read().unwrap() {
+            crossterm::event::Event::Key(key_event) => {
+                tx.send(Event::Input(key_event)).unwrap();
             }
-        } else if let Some(layer2) = &packet.layer_2 {
-            match layer2 {
-                PacketsData::ArpPacket(_) => Some("ARP"),
-                _ => None,
-            }
-        } else {
-            None
+            _ => {}
         }
-    }
-
-    fn render_chart(&mut self, frame: &mut Frame, area: Rect) {
-        let mut protocol_counts: HashMap<&'static str, u32> = HashMap::new();
-
-        for packet in &self.packets {
-            if let Some(protocol_name) = Self::get_last_layer_protocol_name(packet) {
-                *protocol_counts.entry(protocol_name).or_insert(0) += 1;
-            }
-        }
-
-        if protocol_counts.is_empty() {
-            let placeholder = Paragraph::new("Nenhum pacote capturado.")
-                .block(
-                    Block::default()
-                        .title("Pacotes Capturados")
-                        .borders(Borders::ALL),
-                )
-                .alignment(Alignment::Center);
-            frame.render_widget(placeholder, area);
-            return;
-        }
-
-        let mut sorted_protocols: Vec<&&str> = protocol_counts.keys().collect();
-        sorted_protocols.sort_unstable();
-
-        let chart_data: Vec<(&str, u64)> = sorted_protocols
-            .into_iter()
-            .map(|name| (*name, protocol_counts[name] as u64))
-            .collect();
-
-        let max_count = chart_data
-            .iter()
-            .map(|&(_, count)| count)
-            .max()
-            .unwrap_or(0);
-
-        let barchart = BarChart::default()
-            .block(
-                Block::default()
-                    .title("Pacotes Capturados")
-                    .borders(Borders::ALL),
-            )
-            .data(&chart_data)
-            .bar_width(5)
-            .bar_style(Style::default().fg(Color::LightCyan))
-            .value_style(
-                Style::default()
-                    .fg(Color::Black)
-                    .bg(Color::LightCyan)
-                    .add_modifier(Modifier::BOLD),
-            )
-            .label_style(Style::default().fg(Color::White))
-            .max(max_count);
-
-        frame.render_widget(barchart, area);
     }
 }
